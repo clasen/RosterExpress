8e5b1edda95ac3ac59c2cb210d8a19d4454602ba
clasen <martin.clasen@gmail.com>
2024-09-25 09:18:59 -0300

    add demo

--- demo/server.js
+++ demo/server.js
@@ -1,7 +1,9 @@
 const Roster = require('../index.js');
+const path = require('path');
 
 const roster = new Roster({
     maintainerEmail: 'admin@example.com',
+    wwwPath: path.join(__dirname, '..','demo_www'),
 });
 
 roster.start();
